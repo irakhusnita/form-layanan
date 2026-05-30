d39bcc41d43759603542eaafe35d70c7aa74e2c5
irakhusnita <irakhusnita98@gmail.com>
2026-05-30 14:39:50 +0700

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+function cekStatusAdmin(){
+  const now = new Date();
+  const jam = now.getHours();
+
+  const status = document.getElementById("statusAdmin");
+
+  if(jam >= 7 && jam < 14){
+    status.innerHTML = "🟢 Admin Online";
+    status.style.color = "#27ae60";
+  }else{
+    status.innerHTML = "🔴 Admin Offline";
+    status.style.color = "#e74c3c";
+  }
+}
+
+cekStatusAdmin();
+
 function showPopup(){
   setTimeout(function(){
     document.getElementById("popup").style.display = "block";
